debacc2261c62ba053f6ad2d89bb8add65eba156
nmangali226 <165035117+nmangali226@users.noreply.github.com>
2024-12-27 14:59:45 +0530

    Update tip-calculator.py

--- tip-calculator.py
+++ tip-calculator.py
@@ -1,6 +1,19 @@
+## This script will split bill accross members
+
+# Total bill for the dinner
 total_bill = int(input("What was the total bill?\n"))
+
+#tip amount you want to add
 tip_amount = int(input("What is the tip you want to add as a percentage?\n"))
+
+#number of peoples you want to split across
 total_number_of_peoples = int(input("total number of peoples bill to be split?\n"))
+
+# Calculating tip percentage over the total bill
 tip_percentage = (tip_amount * total_bill)/100
+
+#calculating final bill for each person
 final_bill = (total_bill + tip_percentage)/total_number_of_peoples
-print("The final bill per each person is : "+ str(round(final_bill, 2)))
+
+# Print the final bill for everyone
+print("The final bill per each person is : " + str(round(final_bill, 2)))
